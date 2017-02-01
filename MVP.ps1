232de260076b31e4148416e210c27a60a431ccb1
François-Xavier Cat <fxcat@lazywinadmin.com>
2017-02-01 17:11:45 -0500

    Add support for ProfileID

--- MVP.ps1
+++ MVP.ps1
@@ -13,7 +13,11 @@ Set-MVPConfiguration -PrimaryKey '<key>' -AuthorizationCode '<Auth>'
 function Get-MVPProfile
 {
     [CmdletBinding()]
-    PARAM()
+    PARAM(
+        [System.String]$ProfileID
+    )
+
+
     if (-not ($global:MVPPrimaryKey -and $global:MVPAuthorizationCode))
     {
 	    Write-Warning -Message "You need to use Set-MVPConfiguration first to set the Primary Key"
@@ -21,12 +25,32 @@ function Get-MVPProfile
     }
 
 
-    Invoke-RestMethod -Uri "https://mvpapi.azure-api.net/mvp/api/profile?" -Headers @{
-        "Ocp-Apim-Subscription-Key" = $global:MVPPrimaryKey
-        Authorization = $Global:MVPAuthorizationCode
+    #Splat
+    $Splat = @{
+        Uri = "https://mvpapi.azure-api.net/mvp/api/profile?"
+        Headers = @{
+            "Ocp-Apim-Subscription-Key" = $global:MVPPrimaryKey
+            Authorization = $Global:MVPAuthorizationCode}
+    }
 
-    } -Verbose
+    #ProfileID
+    if ($ProfileID)
+    {
+        $Splat.Uri = "https://mvpapi.azure-api.net/mvp/api/profile/$ProfileID"
+    }
+
+
+    Invoke-RestMethod @Splat
+
+    
 }
 
+# Current user
 Get-MVPProfile
+
+# Francois-Xavier Cat
+Get-MVPProfile -ProfileID 5000475
+
+# Emin Atac
+Get-MVPProfile -ProfileID 5000890
 
