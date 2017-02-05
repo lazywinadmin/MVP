66e13e1097d5fbdba646077170056de19ae8190e
Emin <emin.atac@gmail.com>
2017-02-05 22:54:21 +0100

    Adding OAuth code

--- MVP.ps1
+++ MVP.ps1
@@ -15,7 +15,90 @@ function Set-MVPConfiguration
 Set-MVPConfiguration -PrimaryKey '<key>' -AuthorizationCode '<Auth>'
 
 
+# https://blogs.technet.microsoft.com/heyscriptingguy/2013/07/01/use-powershell-3-0-to-get-more-out-of-windows-live/
+# see also Authorization code grant flow https://msdn.microsoft.com/en-us/library/hh243647.aspx
 
+Function Get-OAuthAutorizationCode {
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory)]
+    [String]$ClientID,
+
+    [Parameter(Mandatory)]
+    $SubscriptionKey
+)
+Begin {
+
+    $scope = 'wl.emails%20wl.basic%20wl.offline_access%20wl.signin'
+    $RedirectUri  = 'https://login.live.com/oauth20_desktop.srf'
+    $AuthorizeUri = 'https://login.live.com/oauth20_authorize.srf'
+    $u1 = '{0}?client_id={1}&redirect_uri={2}&response_type=code&scope={3}' -f $AuthorizeUri,$ClientID,$RedirectUri,$scope
+
+    Function Show-OAuthWindow {
+    [CmdletBinding()]
+    Param(
+        [Uri]$url
+    )
+    Begin {
+        # from https://raw.githubusercontent.com/1RedOne/PSWordPress/master/Private/Show-oAuthWindow.ps1
+    }
+    Process {
+        try {
+            Add-Type -AssemblyName System.Windows.Forms -ErrorAction Stop
+            $form = New-Object -TypeName System.Windows.Forms.Form -Property @{Width=440;Height=640}
+            $web  = New-Object -TypeName System.Windows.Forms.WebBrowser -Property @{Width=420;Height=600;Url=$url}
+            # define $uri in the immediate parent scope: 1
+            $DocComp  = {
+                $global:uri = $web.Url.AbsoluteUri
+                if ($global:uri -match 'error=[^&]*|code=[^&]*') {
+                    $form.Close()
+                }
+            }
+            $web.ScriptErrorsSuppressed = $true
+            $web.Add_DocumentCompleted($DocComp)
+            $form.Controls.Add($web)
+            $form.Add_Shown({$form.Activate()})
+            $null = $form.ShowDialog()
+            # set a the autorization code globally
+            $global:AutorizationCode = ([regex]'^\?code=(?<code>.+)&lc=\d{1,10}$').Matches(([uri]$uri).query).Groups | Select -Last 1 -Expand value
+            Write-Verbose -Message "Successfully got authorization code $($AutorizationCode)"
+        } catch {
+            Throw $_
+        }
+    }
+    End {}
+    }
+
+}
+Process {
+
+    Show-OAuthWindow -url $u1
+    
+    if ($AutorizationCode) {
+    
+        $HT = @{
+            Uri = 'https://login.live.com/oauth20_token.srf'
+            Method = 'Post'
+            ContentType = 'application/x-www-form-urlencoded'
+            Body = 'client_id={0}&redirect_uri={1}&client_secret={2}&code={3}&grant_type=authorization_code' -f  $ClientID,$RedirectUri,$SubscriptionKey,$AutorizationCode
+        }
+
+        try {
+            $Response = Invoke-RestMethod @HT -ErrorAction Stop
+
+            Write-Verbose -Message 'Successfully got access token'
+
+            Set-MVPConfiguration -PrimaryKey $SubscriptionKey -AuthorizationCode ('{0} {1}' -f $Response.token_type,$Response.access_token)
+
+        } catch {
+            Throw $_
+        }
+    } else {
+        Write-Warning -Message "No authorization code set"
+    }
+}
+End {}
+}
 
 
 
