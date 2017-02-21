45e27367ad76b3af7c23409f37b2998cfdb17e2e
Emin <emin.atac@gmail.com>
2017-02-21 22:11:48 +0100

    Add some help blocks in the functions Add try/catch blocks Add some messages for the verbose stream Add typed parameters

--- MVP/Public/Set-MVPConfiguration.ps1
+++ MVP/Public/Set-MVPConfiguration.ps1
@@ -1,14 +1,45 @@
-﻿function Set-MVPConfiguration
-{
-<#
-Set-MVPConfiguration -PrimaryKey '<key>' -AuthorizationCode '<Auth>'
-#>
-	[CmdletBinding()]
-	PARAM (
-        $ClientID,
-        $SubscriptionKey)
-	
-    $output = Get-MVPOAuthAutorizationCode -ClientID $ClientID -SubscriptionKey $SubscriptionKey
-    $global:MVPPrimaryKey = $SubscriptionKey
-    $global:MVPAuthorizationCode = ('{0} {1}' -f $output.token_type,$output.access_token)
+﻿Function Set-MVPConfiguration {
+[CmdletBinding()]
+PARAM (
+    [Parameter()]
+    [string]$ClientID='0000000048193351',
+
+    [Parameter(Mandatory)]
+    [string]$SubscriptionKey
+)
+Begin {}
+Process {
+    try {
+        $output = Get-MVPOAuthAutorizationCode -ClientID $ClientID -SubscriptionKey $SubscriptionKey -ErrorAction Stop
+        Write-Verbose -Message 'Successfully call the Get-MVPOAuthAutorizationCode function'
+    } catch {
+        Write-Warning -Message 'Something went wrong with the private function Get-MVPOAuthAutorizationCode'
+    }
+    if ($output) {
+        $global:MVPPrimaryKey = $SubscriptionKey
+        $global:MVPAuthorizationCode = ('{0} {1}' -f $output.token_type,$output.access_token)
+        Write-Verbose -Message 'Successfully set the global variables MVPPrimaryKey and MVPAuthorizationCode'
+    } else {
+        Write-Warning -Message 'Failed to define an MVPAuthorizationCode variable'
+    }
 }
+End {}
+}
+<#
+    .SYNOPSIS
+        Get an Oauth Autorization code
+
+    .DESCRIPTION
+        Call the private Get-MVPOAuthAutorizationCode function and define both an MVPPrimaryKey and MVPAuthorizationCode global variable
+
+    .PARAMETER SubscriptionKey
+        It's the primary key or secondary key you get in your profile on this page https://mvpapi.portal.azure-api.net/developer
+
+    .PARAMETER ClientID
+        It's the clientID you see in the url of the MVPAuth application on your https://account.live.com/consent/Manage page
+
+    .EXAMPLE
+
+    Set-MVPConfiguration -SubscriptionKey $myKey
+
+#>
