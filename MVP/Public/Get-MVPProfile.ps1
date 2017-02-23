1df0e79cebf556e258c3b71f77532330f8afb8f4
Emin <emin.atac@gmail.com>
2017-02-23 19:24:51 +0100

    Change the private function Get-MVPOAuthAutorizationCode to handle the validity of the OAuth 2.0 access token Ask for permission(s) only if required. Change for example the Get-MVPProfile public function to call the front-end Set-MVPConfiguration function to handle gracefully the Oauth token whenever required

--- MVP/Public/Get-MVPProfile.ps1
+++ MVP/Public/Get-MVPProfile.ps1
@@ -33,15 +33,17 @@ Begin {}
 Process {
     if (-not ($global:MVPPrimaryKey -and $global:MVPAuthorizationCode)) {
 
-	    Write-Warning -Message "You need to use Set-MVPConfiguration first to set the Primary Key"
+	    Write-Warning -Message 'You need to use Set-MVPConfiguration first to set the Primary Key'
 	    
     } else {
+        
+        Set-MVPConfiguration -SubscriptionKey $MVPPrimaryKey
 
         $Splat = @{
             Uri = 'https://mvpapi.azure-api.net/mvp/api/profile?'
             Headers = @{
                 'Ocp-Apim-Subscription-Key' = $global:MVPPrimaryKey ;
-                Authorization = $Global:MVPAuthorizationCode
+                Authorization = $global:MVPAuthorizationCode
             }
             ErrorAction = 'Stop' ;
         }
