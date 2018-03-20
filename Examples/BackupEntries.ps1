37bfb5b8f9445b69f82efb4b315921726dfb3058
François-Xavier Cat <fxcat@lazywinadmin.com>
2018-03-19 21:21:30 -0700

    Add example to backup entries

---
+++ Examples/BackupEntries.ps1
@@ -0,0 +1,30 @@
+﻿# Backup Contribution
+Get-MVPContribution -Verbose -all | ConvertTo-Json |
+Out-File -FilePath BackupMVP_$(Get-Date -format 'yyyyMMdd_HHmmss').txt
+
+# Retrieve the type of contribution
+Get-MVPContributionType
+
+# Retrieve the ContributionTechnology
+(Get-MVPContributionArea).contributionarea.name
+# simple backup
+Get-MVPContribution -limit 1000 |
+ConvertTo-Json |
+Out-File -FilePath BackupMVP_$(Get-Date -format 'yyyyMMdd_HHmmss').json
+
+# per year
+Get-MVPContribution -limit 1000 |
+select-object -Property *,@{L='Year';E={$_.startdate.substring(0,4)}} |
+Group-Object -Property year |
+Foreach-object -Process {
+    # Grab the current year
+    $Year = $_.name
+
+    # Get the contribution of the current year
+    $ContributionsOfYear = $_.Group
+
+    # Save to file
+    $ContributionsOfYear |
+    ConvertTo-Json |
+    Out-File -FilePath BackupMVP_Year$Year-$(Get-Date -format 'yyyyMMdd_HHmmss').json
+}
